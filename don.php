e7b3eac55076ec88f67f3cdbbd4991fce87e9e8b
gengeal <kevin.renambatz@uha.fr>
2014-12-05 00:55:34 +0100

    tab

--- don.php
+++ don.php
@@ -11,7 +11,7 @@
 				   <ul class="liste_ligne" >
 						<li> Dons directs </br> Ce système de dons permet de contribuer financièrement </br> 
 						<a href="dons_direct.php" > Cliquez ici pour faire un don </a>  </li> 
-						<li> Dons indirects </br> Ce système de dons permet de contribuer en jouant à un jeu.
+						<li> Dons indirects </br> Ce système de dons permet de contribuer en jouant à un jeu. </br>
 						 Le site s'engage à transformer votre score en donation </br> <a href="dons_indirect.php" > Cliquez ici pour commencer à jouer </a> </li>
 					</ul>
 					
