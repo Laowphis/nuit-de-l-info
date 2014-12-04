ed986e962b2f3817a1d82353ca059f99f8b6c85c
gengeal <kevin.renambatz@uha.fr>
2014-12-04 20:11:13 +0100

    dons_fond
    3 fichiers = 1 socle + 2 sous pages

--- don.php
+++ don.php
@@ -1,28 +1,33 @@
 <div id="contenu">
 					
 	<h1>Un don pour changer le monde </h1> <!-- Title -->
-	<h3> Dans l'optique de la réduction des inégalités dans le monde, notre site met en place deux systèmes permettant de contribuer financièrement 
-	à différentes associations engagées dans ces luttes humanitaires.   </h3>  <!-- Presentation -->
+		<h3> Dans l'optique de la réduction des inégalités dans le monde, notre site met en place deux systèmes permettant de contribuer financièrement 
+		à différentes associations engagées dans ces luttes humanitaires.   </h3>  <!-- Presentation -->
  
-	<p> Nous proposons un système de donations directes et un système de donations indirectes </p>
-		<table>  <!-- Table for the donation's system -->
+		<p> Nous proposons un système de donations directes et un système de donations indirectes </p>
+			<table>  <!-- Table for the donation's system -->
 
-			<tr>   
-				<td>Dons directs</td>
-				<td>Dons indirects</td>
+				<tr>   
+					<td>Dons directs</td>
+					<td>Dons indirects</td>
 		
-			</tr>
+				</tr>
 	
-			<tr>
-				<td> Ce système de dons permet de contribuer financièrement </td>
-				<td> Ce système de dons permet de contribuer en jouant à un jeu. </br> Le site s'engage de transformer un pourcentage de votre score en donation </td>
+				<tr>
+					<td> Ce système de dons permet de contribuer financièrement </td>
+					<td> Ce système de dons permet de contribuer en jouant à un jeu. </br> Le site s'engage de transformer un pourcentage de votre score en donation </td>
 					
-			</tr>	
+				</tr>	
 				
-			<tr>
-				<td> <a href "" > Cliquez ici pour faire un don </a> </td>
-				<td> <a href "" > Cliquez ici pour commencer à jouer </a> </td>
-			</tr>
+				<tr>
+					<td> <a href="dons_direct.html" > Cliquez ici pour faire un don </a> </td>
+					<td> <a href="dons_indirect.html" > Cliquez ici pour commencer à jouer </a> </td>
+				</tr>
+	
+	
+	
+
+			</table>
 	
 	
 	
