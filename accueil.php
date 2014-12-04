a54f6b44067509c2261c34e3ea7ea861aea1268e
othmannoob <otenich@gmail.fr>
2014-12-04 23:58:12 +0100

    acceuil

--- accueil.php
+++ accueil.php
@@ -10,14 +10,16 @@
 	<p>Que ce soit les conflits armées, le climat ou les catastrophes naturelles, les populations sinistrées dans le monde ont plus que jamais besoin de notre solidarité et de notre aide. Faites un don, du montant de votre choix, et aidez nous dans notre combat</br>
 	Nous receuillons vos dons, et les distribuons aux ONG de vos choix. Nous promouvons les "petites" ONG que vous nous proposez ou dont nous trouvons le travail louable. Nous recevons les demandes des ONG et les diffusons pour que vous voyez concrètement leurs besoins
 	<a href="association.php"><strong>ici</strong></a></p>
-	</hr>
+	
+	</br></br></br>
+	
+	
+	<h2>Carte des populations sinistrées dans le monde</h2></br>
 	
 	<div id="map">
-	<h2>Carte des populations sinistrées dans le monde</h2>
-	<iframe width='85%' height='500px' frameBorder='2' src='https://a.tiles.mapbox.com/v4/wildcerebrus.kd8723da/page.html?access_token=pk.eyJ1Ijoid2lsZGNlcmVicnVzIiwiYSI6IjZsTlpyTGcifQ.DNivhP6lPb55hN0QQhJJyw#3/0.00/0.00'></iframe>
+	<iframe width='100%' height='650px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/wildcerebrus.kd8723da/page.html?access_token=pk.eyJ1Ijoid2lsZGNlcmVicnVzIiwiYSI6IjZsTlpyTGcifQ.DNivhP6lPb55hN0QQhJJyw#3/0.00/0.00'></iframe>
 	</div>
 	
-	</hr>
 	<table>
 		<tr>
 			<td>Convaincu de faire un don ? C'est par ici</td>
