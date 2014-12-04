7eaf0ccb81841c813111607930aaa2a0507573a2
gengeal <kevin.renambatz@uha.fr>
2014-12-05 00:35:50 +0100

    v

--- don.php
+++ don.php
@@ -7,50 +7,18 @@
 		<p= "para_centre"> Nous proposons un système de donations directes et un système de donations indirectes </p>
 			
 		<p>
-<<<<<<< HEAD
+
 				   <ul class="liste_ligne">
-						<li> Dons directs </li> 
-						<li> Dons indirects </li>
+						<li> Dons directs <br> Ce système de dons permet de contribuer financièrement <br> 
+						<a href="dons_direct.php" > Cliquez ici pour faire un don </a>  </li> 
+						<li> Dons indirects </br> Ce système de dons permet de contribuer en jouant à un jeu.
+						</br> Le site s'engage à transformer votre score en donation </br> <a href="dons_indirect.php" > Cliquez ici pour commencer à jouer </a> </li>
 					</ul>
 					
-					<ul class="liste_ligne">
-					<li>Ce système de dons permet de contribuer financièrement </li>
-					<li>Ce système de dons permet de contribuer en jouant à un jeu. </br> Le site s'engage à transformer votre score en donation </li>
-					</ul>
-=======
-<<<<<<< HEAD
-				   <p class="text_gauche">
-					Dons directs </br>
-					Ce système de dons permet de contribuer financièrement </br>
-					<a href="index.php?page=dons_direct" > Cliquez ici pour faire un don </a>
-		
-				</p>
-	
-				
-					<p class="text_droite">
-=======
-				   <div class="text_gauche">
-					Dons directs 
-					</div>
-					<div class="text_droite">
->>>>>>> origin/master
-					Dons indirects </br>
-					</div>
-					<div class="text_gauche">
-					Ce système de dons permet de contribuer financièrement 
-					</div>
-					<div class="text_droite">
-					Ce système de dons permet de contribuer en jouant à un jeu. </br> Le site s'engage à transformer votre score en donation </br>
-					</div>
->>>>>>> origin/master
 					
 					
-			
-					<ul class= "liste_ligne>
-					<li> <a href="dons_direct.php" > Cliquez ici pour faire un don </a> </li>
 					
 					
-					 <li> <a href="dons_indirect.php" > Cliquez ici pour commencer à jouer </a> </li>
 				
 			
 		</p>
