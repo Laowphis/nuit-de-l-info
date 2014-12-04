74dc9fc3cd89dacc64c2755f10410648731b9339
othmannoob <otenich@gmail.fr>
2014-12-05 00:05:29 +0100

    acceuil

--- accueil.php
+++ accueil.php
@@ -7,9 +7,9 @@
 	<p>Notre organisation soutient des ONG humanitaires dans le monde entier.</br> 
 	Notre site a pour but de vous faire découvrir des ONG peu connues, mais dont les oeuvres sont capitales pour les populations aidées.</p>
 	
-	<p>Que ce soit les conflits armées, le climat ou les catastrophes naturelles, les populations sinistrées dans le monde ont plus que jamais besoin de notre solidarité et de notre aide. Faites un don, du montant de votre choix, et aidez nous dans notre combat</br>
+	<p>Que ce soit les conflits armées, le climat ou les catastrophes naturelles, les populations sinistrées dans le monde ont plus que jamais besoin de notre solidarité et de notre aide. Faites un don, du montant de votre choix, et aidez nous dans notre combat.</br>
 	Nous receuillons vos dons, et les distribuons aux ONG de vos choix. Nous promouvons les "petites" ONG que vous nous proposez ou dont nous trouvons le travail louable. Nous recevons les demandes des ONG et les diffusons pour que vous voyez concrètement leurs besoins
-	<a href="association.php"><strong>ici</strong></a></p>
+	<a href="index.php?page=association"><strong>ici</strong></a>.</p>
 	
 	</br></br></br>
 	
@@ -20,7 +20,7 @@
 	<iframe width='100%' height='650px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/wildcerebrus.kd8723da/page.html?access_token=pk.eyJ1Ijoid2lsZGNlcmVicnVzIiwiYSI6IjZsTlpyTGcifQ.DNivhP6lPb55hN0QQhJJyw#3/0.00/0.00'></iframe>
 	</div>
 	
-	<table>
+	<table id="table">
 		<tr>
 			<td>Convaincu de faire un don ? C'est par ici</td>
 			<td><img src="img/fleche.png"/> <a href="index.php?page=Don"> <img src="img/don.png"/> </a></td>
