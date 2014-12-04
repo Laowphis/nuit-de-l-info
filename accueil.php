e86da49cee3c95ac3fa97f624490a075a1293f99
othmannoob <otenich@gmail.fr>
2014-12-04 20:46:05 +0100

    acceuil

--- accueil.php
+++ accueil.php
@@ -1,11 +1,28 @@
 
-				<div id="contenu">
-					<!-- <embed id="zik" src="music/Hard.mp3"  hidden="true" width="0" height="0" autostart="false" loop="false" align="left"> </embed> -->
-					<audio id="zik" src="" autoplay="true"  > </audio> </br>
-					<p id="accroche"> Bien le bienvenue sur mon site aventurier. </br>Ce site te premetra de me connaitre et de suivre mes projet </p>
-					<h1>Qui suis je?</h1>
-					<p> Etuidant en 1er année à L'ENSISA à Mulhouse en filiére informatique et reseaux </p>
-					<p></p>
-					<p>  </p>
-				</div>
-			
+<div id="contenu">
+	<h1>SUPER-ONG.COM</h1>
+	
+	<h2>Ici, achetez votre place au paradis !</h2>
+	
+	<p>Notre organisation soutient des ONG humanitaires dans le monde entier.</br> 
+	Notre site a pour but de vous faire découvrir des ONG peu connues, mais dont les oeuvres sont capitales pour les populations aidées.</p>
+	
+	<p>Que ce soit les conflits armées, le climat ou les catastrophes naturelles, les populations sinistrées dans le monde ont plus que jamais besoin de notre solidarité et de notre aide. Faites un don, du montant de votre choix, et aidez nous dans notre combat</br>
+	Nous receuillons vos dons, et les distribuons aux ONG de vos choix. Nous promouvons les "petites" ONG que vous nous proposez ou dont nous trouvons le travail louable. Nous recevons les demandes des ONG et les diffusons pour que vous voyez concrètement leurs besoins
+	<a href="association.php"><strong>ici</strong></a></p>
+	</hr>
+	
+	<div id="map">
+	<h2>Carte des populations sinistrées dans le monde</h2>
+	</div>
+	
+	</hr>
+	<table>
+		<tr>
+			<td>Convaincu de faire un don ? C'est par ici</td>
+			<td><img src="img/fleche.png"/> <a href="don.php"> <img src="img/don.png"/> </a></td>
+		<tr>
+	</table>
+	
+	
+</div>
