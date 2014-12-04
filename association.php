90cdfd63bdc67f340468715dd4f36c2581661cab
Lokimir <szymanski.cedric@gmail.com>
2014-12-04 21:12:48 +0100

    Association v0.4

--- association.php
+++ association.php
@@ -1,6 +1,6 @@
 ﻿<div id="contenu">
 	<h1>Les causes humanitaires que nous soutenons </h1>
-	<h2>Action Contre la Faim: ACF</h2>
+	<h2>Action Contre la Faim</h2>
 	<p>Née dans le contexte de la crise afghane en 1979, Action internationale contre la Faim (dénomination de l’association à sa création) a été fondée 
 		par un groupe d'intellectuels français ainsi qu'un certain nombre de médecins, journalistes et écrivains.
 		La mission d'Action contre la Faim consiste gences et de conflits. En 2012 ACF est intervenue dans 45 pays, venant en aide à près de 7,3 millions de personnes.
@@ -15,5 +15,57 @@
 	</p>
 	<img src="./img/ACF.jpg" alt="Action Contre la Faim"><br><br>
 	Site web : <a href="http://www.actioncontrelafaim.org/">http://www.actioncontrelafaim.org/</a>
+	<br><br>
+	
+	<h2>Médecins Sans Frontières</h2>
+	<p>Médecins Sans Frontières a été créée le 20 décembre 1971 par des médecins français qui s'étaient rendus au Biafra avec la Croix-Rouge pour tenter 
+		d'y aider la population lors de la guerre qui avait opposé cette région indépendantiste au gouvernement central nigerian entre 1967 et 1970. 
+		Ils voulurent fonder une association qui allierait aide humanitaire et actions de sensibilisation auprès des 
+		médias et des institutions politiques. Médecins Sans Frontières interviennent lors de :
+		<br>
+		<ul>
+			<li>Populations victimes de conflits</li>
+			<li>Epidémies, endémies, pandémies</li>
+			<li>Catastrophes naturelles</li>
+			<li>Exclusion des soins</li>
+		</ul>
+	</p>
+	<img src="./img/MSF.jpg" alt="Médecins Sans Frontières"><br><br>
+	Site web : <a href="http://www.msf.fr/">http://www.msf.fr/</a>
+	<br><br>
+		
+	<h2>Enfants Du Monde</h2>
+	<p>Un développement sain pour un enfant requiert bien sûr plusieurs domaines d’intervention complémentaires et indissociables.
+		Depuis plusieurs années, Enfants Du Monde avons mis l’accent sur la lutte contre la malnutrition, difficile pour un enfant de grandir, 
+		guérir et apprendre sans manger à sa faim. Enfants du Monde s’est donc spécialisée dans la distribution de compléments alimentaires, 
+		tout en portant en parallèle une attention particulière à des projets dans des domaines connexes tels :
+		<br>
+		<ul>
+			<li>Constructions de cantines</li>
+			<li>Développement des écoles</li>
+			<li>Soutien aux enfants sourds</li>
+			<li>Accès à l'eau potable</li>
+		</ul>
+	</p>
+	<img src="./img/EDM.jpg" alt="Enfants du Monde"><br><br>
+	Site web : <a href="http://www.edm.org/">http://www.edm.org/</a>
+	<br><br>
+	
+	<h2>De l'Eau Pour L'Afrique</h2>
+	<p>L’association De l’eau pour l’Afrique est une association qui favorise l’accès à l’eau potable dans les pays en voie de développement.
+		Malgré son jeune âge et le peu de budget dont elle dispose, l’association gravit peu à peu les marches et commence à grandir dans le monde de l’Humanitaire.
+		Elle s'occupe essentiellement du traitement de l'eau et effectue leur mission sur les pays touchés par les problèmes d'approvisionnement et de distribution 
+		et d'accès d'eau potable. Les aides que l'association propose :
+		<br>
+		<ul>
+			<li>Accès à l'eau potable</li>
+			<li>Creuser des puits</li>
+			<li>Accès à l'eau pour l'agriculture</li>
+			<li>Information face aux dangers du réassèchements du sol</li>
+		</ul>
+	</p>
+	<img src="./img/EDA.jpg" alt="Eau pour l'Afrique"><br><br>
+	Site web : <a href="http://www.deleaupourlafrique.org/">http://www.deleaupourlafrique.org/</a>
+	<br><br>
 </div>
 			
