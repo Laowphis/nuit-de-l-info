5455b993d0255f797e3f81cf3bd9df635e2e5602
Lokimir <szymanski.cedric@gmail.com>
2014-12-04 18:32:17 +0100

    Association v0.1

--- association.php
+++ association.php
@@ -1,11 +1,16 @@
-
-				<div id="contenu">
-					<!-- <embed id="zik" src="music/Hard.mp3"  hidden="true" width="0" height="0" autostart="false" loop="false" align="left"> </embed> -->
-					<audio id="zik" src="" autoplay="true"  > </audio> </br>
-					<p id="accroche"> Bien le bienvenue sur mon site aventurier. </br>Ce site te premetra de me connaitre et de suivre mes projet </p>
-					<h1>Qui suis je?</h1>
-					<p> Etuidant en 1er année à L'ENSISA à Mulhouse en filiére informatique et reseaux </p>
-					<p></p>
-					<p>  </p>
-				</div>
+﻿<div id="contenu">
+	<h1>Les différentes causes humanitaires </h1>
+	<h2>Action contre la faim: ACF</h2>
+	<p>La mission d'Action contre la Faim consiste à sauver des vies par la prévention, la détection et le traitement de la sous-nutrition,
+		en particulier pendant et suite à des situations d’urgences et de conflits. Elle centre son intervention sur une approche intégrée, 
+		prenant en compte divers secteurs :
+		<br>
+		<ul>
+			<li>Nutrition, santé et pratiques de soins</li>
+			<li>Sécurité alimentaire et moyens d’existence</li>
+			<li>Eau, assainissement et hygiène</li>
+			<li>Plaidoyer et sensibilisation</li>
+		</ul>
+	</p>
+</div>
 			
