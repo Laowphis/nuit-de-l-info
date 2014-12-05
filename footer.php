897021ad63b4a649e7a88416d9f24281a642a5a2
WildCerebrus <mathieu.althuser@gmail.com>
2014-12-05 03:59:13 +0100

    Footer #2
    Test 2

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 </div>
 			<div id="footer">
-				<img id="ensisa" src="img/logoENSISA.gif" alt="logo ecole" /> </h1>
-				<ul id="profil02">
-					<li>L'équipe BSoD()</li>
-					<li>Les images appartiennes à leurs ayant droits</li>
+				
+				<ul id="profil02" list-style-type="none">
 					<li>Ce site est pûrement fictif, toute somme versée ne saurait être localisée avec certitude</li>
+					<li>Les images appartiennes à leurs ayant droits</li>
 					<li>mail: insertcoin4charity@gmail.com</li>
+					<li>L'équipe BSoD()</li>
 				</ul>
 			</div>
 		</div>
