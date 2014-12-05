96eaa6d41ce51cf42d2be9343341c99acf7e0654
WildCerebrus <mathieu.althuser@gmail.com>
2014-12-05 04:05:28 +0100

    Footer back

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </div>
 			<div id="footer">
 				
-				<ul id="profil02" list-style-type="none">
+				<ul>
 					<li>Ce site est pûrement fictif, toute somme versée ne saurait être localisée avec certitude</li>
 					<li>Les images appartiennes à leurs ayant droits</li>
 					<li>mail: insertcoin4charity@gmail.com</li>
