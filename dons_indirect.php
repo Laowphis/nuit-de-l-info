eee5996c728678a94bd4cfebe9e693e3f7e2a431
WildCerebrus <mathieu.althuser@gmail.com>
2014-12-05 02:11:51 +0100

    2048_2

--- dons_indirect.php
+++ dons_indirect.php
@@ -8,13 +8,13 @@
 	Bon jeu à vous !
 	</p>
 	
-	<p><a href="2048.php"></p>
+	<p><a href="2048.php">Le site s'engage à transformer votre score en donation.</a></p>
 	
 	
 	<!--<div id= "jeu" >
 	
 	
 	</div>-->
-<p class= "para_centre"> Le site s'engage à transformer votre score en donation. </p>
+
 
 </div>
