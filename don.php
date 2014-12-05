1386a3ba462c5f3dec24bcebd726ad9e34170683
WildCerebrus <mathieu.althuser@gmail.com>
2014-12-05 01:23:10 +0100

    Erreur don

--- don.php
+++ don.php
@@ -10,9 +10,9 @@
 
 				   <ul class="liste_ligne" >
 						<li> Dons directs </br> Ce système de dons permet de contribuer financièrement </br> 
-						<a href="dons_direct.php" > Cliquez ici pour faire un don </a>  </li> 
+						<a href="index.php?dons_direct" > Cliquez ici pour faire un don </a>  </li> 
 						<li> Dons indirects </br> Ce système de dons permet de contribuer en jouant à un jeu. </br>
-						   <a href="dons_indirect.php" > Cliquez ici pour commencer à jouer </a> </li>
+						   <a href="index.php?dons_indirect" > Cliquez ici pour commencer à jouer </a> </li>
 					</ul>
 					
 					
